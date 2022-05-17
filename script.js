f1d7ac84e6258d51a257b5dd9c08f44848252793
SzilviaFici <sziszfici@gmail.com>
2022-05-17 22:08:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let {
+    wurfsummeS1 = 0;
+    wurfsummeS2 = 0
+}
+
+function wurf () {
+
+    let wurf1 = Math.floor(Math.random())
+    let wurf2 = Math.floor(Math.random())
+
+    wuerfelnS1
+    document.getElementById("anzahl1").innerHTML = Anzahl Würfe Spieler 1
+if (wurf1 + wurf2 === 7){
+    
+}
+}
+
